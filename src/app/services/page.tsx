7837a02076bb3e9d52b4b8894530b7d78ea300d8
Marvinpena02828 <129364396+Marvinpena02828@users.noreply.github.com>
2026-04-20 17:56:47 +0800

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,11 +4,11 @@ import Banner from "./Components/Banner";
 import ListOfServices from "./Components/ListOfServices";
 
 export const metadata: Metadata = {
-  title: "AyyanTech Services – Smart Tech & Electronics Solutions",
+  title: "TechOn Services – Smart Tech & Electronics Solutions",
   description:
-    "Explore AyyanTech's professional services including R&D, product design, manufacturing, and global distribution of innovative smartphone accessories.",
+    "Explore TechOn's professional services including R&D, product design, manufacturing, and global distribution of innovative smartphone accessories.",
   alternates: {
-    canonical: "https://ayyantech.net/services",
+    canonical: "https://tech-on.net/services",
   },
 };
 
