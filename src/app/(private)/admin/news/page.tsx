b39ff3e79ac9462335092ff3ddf1f3ab18a6f757
Marvinpena02828 <129364396+Marvinpena02828@users.noreply.github.com>
2026-04-20 14:25:15 +0800

    Update page.tsx

--- src/app/(private)/admin/news/page.tsx
+++ src/app/(private)/admin/news/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import NewsContent from "./NewsContent";
-
+ 
 export default async function NewsPage() {
   const supabase = await createClient();
   const {
