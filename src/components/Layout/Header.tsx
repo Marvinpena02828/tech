299a07426abe11b112d4d4005348c187697a38e6
Marvinpena02828 <129364396+Marvinpena02828@users.noreply.github.com>
2026-05-06 19:25:38 +0800

    Update Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,7 +10,7 @@ import { getPublicCategories } from "@/app/(private)/admin/categories/models/cat
 import { Category } from "@/lib/types";
 import { createClient } from "@/lib/supabase/client";
 import { usePageTranslation } from "@/app/hooks/usePageTranslation";
-
+ 
 interface LogosProps {
   main?: string;
   mobile?: string;
